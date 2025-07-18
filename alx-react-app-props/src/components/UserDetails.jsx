a394ea7e004f2b40e14ac2bab3f44375923eac2c
Gugu Mthethwa <pociamthethwa@gmail.com>
2025-07-18 22:56:14 +0200

    Update UserDetails.jsx

--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,8 +1,10 @@
+import UserContext from "./UserContext";
+
 function UserDetails({ userData }) {
   return (
     <div>
-      <p>Name: {userData.name}</p>
-      <p>Email: {userData.email}</p>
+      <p>Name: {UserContext.name}</p>
+      <p>Email: {UserContext.email}</p>
     </div>
   );
 }
