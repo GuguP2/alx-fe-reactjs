63a6c3f0d5ba5a187ce04d199c2b7611b0563ab0
Gugu Mthethwa <pociamthethwa@gmail.com>
2025-07-31 23:08:41 +0200

    new

--- github-user-search/vite.config.js
+++ github-user-search/vite.config.js
@@ -5,3 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+const GITHUB_API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY;
+
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `Bearer ${GITHUB_API_KEY}`,
+  },
+})
+  .then((res) => res.json())
+  .then((data) => console.log(data));
+
