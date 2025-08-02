7b484223e057fde54b047a2559f4d185e8a01d6a
Gugu Mthethwa <161052046+GuguP2@users.noreply.github.com>
2025-08-02 22:02:24 +0200

    Add files via upload

---
+++ github-user-search/src/services/githubApi.js
@@ -0,0 +1,14 @@
+// src/services/githubService.js
+import axios from 'axios';
+
+const GITHUB_API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY;
+
+export async function fetchUserData(username) {
+  const response = await axios.get(`https://api.github.com/users/${username}`, {
+    headers: {
+      Authorization: `Bearer ${GITHUB_API_KEY}`,
+    },
+  });
+
+  return response.data;
+}
