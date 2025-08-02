c7d05e85e4baabd7d3d4214cb02378df3cccb189
Gugu Mthethwa <pociamthethwa@gmail.com>
2025-08-02 21:33:37 +0200

    token

---
+++ github-user-search/src/home.jsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <h2>🏠 Welcome to the GitHub App</h2>;
+}
+
+export default Home;
---
+++ github-user-search/src/profile.jsx
@@ -0,0 +1,5 @@
+function Profile() {
+  return <h2>👤 GitHub Profile Viewer</h2>;
+}
+
+export default Profile;
