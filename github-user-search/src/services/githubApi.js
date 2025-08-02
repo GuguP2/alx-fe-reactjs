cd06096449c0fb17e6853c553d1979fbaa04ca08
Gugu Mthethwa <pociamthethwa@gmail.com>
2025-08-02 23:33:22 +0200

    ;

--- github-user-search/src/services/githubApi.js
+++ github-user-search/src/services/githubApi.js
@@ -1,14 +1,16 @@
-// src/services/githubService.js
-import axios from 'axios';
-
 const GITHUB_API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY;
 
-export async function fetchUserData(username) {
-  const response = await axios.get(`https://api.github.com/users/${username}`, {
+export async function fetchGitHubProfile(username) {
+  const response = await fetch(`https://api.github.com/users/${username}`, {
     headers: {
       Authorization: `Bearer ${GITHUB_API_KEY}`,
     },
   });
 
-  return response.data;
+  if (!response.ok) {
+    throw new Error('GitHub API request failed');
+  }
+
+  return await response.json();
 }
+export default GITHUB_API_KEY;
