0f604e20b85f314be0cc54a29b1b71ff645c9e4c
Gugu Mthethwa <pociamthethwa@gmail.com>
2025-08-08 15:31:59 +0200

    new

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 
 function UserProfile() {
   return (
-    <div className="user-profile" bg-gray-100 md:p-8 sm:p-4 max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:text-sm md:text-base sm:w-24 md:w-36 md:h-36 max-w-xs md:max-w-sm text-lg md:text-xl sm:h-24>
-      <img src="https://via.placeholder.com/150" alt="User" rounded-full w-36 h-36 mx-auto/>
-      <h1 text-xl text-blue-800 my-4>John Doe</h1>
+    <div className="user-profile" bg-gray-100 md:p-8 sm:p-4 max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:text-sm md:text-base sm:w-24 md:w-36 md:h-36 max-w-xs md:max-w-sm text-lg md:text-xl sm:h-24 hover:shadow-xl>
+      <img src="https://via.placeholder.com/150" alt="User" rounded-full w-36 h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out/>
+      <h1 text-xl text-blue-800 my-4 hover:text-blue-500>John Doe</h1>
       <p text-gray-600 text-base>Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
