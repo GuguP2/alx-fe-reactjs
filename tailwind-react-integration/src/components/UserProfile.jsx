47bae8ac1b3158899fbac38ce944758f1cba00b2
Gugu Mthethwa <pociamthethwa@gmail.com>
2025-08-08 15:19:22 +0200

    SCREEN SIZES

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 
 function UserProfile() {
   return (
-    <div className="user-profile" bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg>
+    <div className="user-profile" bg-gray-100 p-4 on sm: screens and p-8 on md: screens and above max-w-sm mx-auto my-20 rounded-lg shadow-lg>
       <img src="https://via.placeholder.com/150" alt="User" rounded-full w-36 h-36 mx-auto/>
       <h1 text-xl text-blue-800 my-4>John Doe</h1>
       <p text-gray-600 text-base>Developer at Example Co. Loves to write code and explore new technologies.</p>
